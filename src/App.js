459ffc1a4f2b00ad7a289769daec405a290b5f36
CarlosSantiago Nevett <csnevett@gmail.com>
2021-01-19 20:48:58 -0500

    finishedmvp

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@
 */
 
 // Import the state hook
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
-import Posts from './components/Posts/Posts'
-import SearchBar from './components/SearchBar/SearchBar'
+import Posts from './components/Posts/Posts';
+import post from './components/Posts/Post';
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
-import dummyData from './dummy-data'
+import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
-  const [posts, setPosts] = useState(dummyData)
+  const [posts, setPosts] = useState(dummyData);
+
+  console.log(posts)
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
-  const likePost = postId => {
+  const likePost = (postId) => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -31,20 +34,21 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    setPosts(posts.map((post) => {
-      return post.id = postId
-      ? {...dummyData, likes: dummyData.likes+1}
-      : dummyData; 
-    })
+    setPosts(
+      posts.map((post) => {
+        return (post.id === postId
+          ? { ...post, likes: (post.likes)+1}
+          : post);
+      })
     );
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* Add SearchBar and Posts here to render them */}
-      {<Posts likePost={likePost} posts={posts}/>}
+      {<SearchBar />} 
+      {<Posts likePost={likePost} posts={posts} />}
       {/* Check the implementation of each component, to see what props they require, if any! */}
-      {<SearchBar/>}
     </div>
   );
 };
